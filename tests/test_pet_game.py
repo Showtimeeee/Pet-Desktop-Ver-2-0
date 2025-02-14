7c81021179eec170032ab565663d69e8b13e00bc
Sipovich <94108043+Showtimeeee@users.noreply.github.com>
2025-02-14 11:33:22 +0300

    Update test_pet_game.py

--- tests/test_pet_game.py
+++ tests/test_pet_game.py
@@ -17,3 +17,15 @@ def test_pet_slow_down():
     pet.slow_down(None)
     assert pet.speed == 10
     pet.window.destroy()
+
+def test_quit_program():
+    pet = Pet('gifs/dasad.gif', 10)
+    pet.quit_program()
+    assert pet.window.winfo_exists() == 0
+
+def test_frame_change():
+    pet = Pet('gifs/dasad.gif', 10)
+    initial_frame_index = pet.frame_index
+    pet.changetime(pet.moveright)
+    assert pet.frame_index != initial_frame_index
+    pet.window.destroy()
